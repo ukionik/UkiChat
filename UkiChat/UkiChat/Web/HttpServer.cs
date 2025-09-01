6d96f6fb9238b0a848b0ffa8a84820941224efff
Vyzhanov Andrey <ukionik@yandex.ru>
2025-09-01 23:32:27 +0300

    Добавлен билд ukichat-web в wwwroot и вывод в UkiChat WPF через Http сервер

---
+++ UkiChat/UkiChat/Web/HttpServer.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+
+namespace UkiChat.Web;
+
+public class HttpServer(int port = 5000)
+{
+    public void Start()
+    {
+        var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseUrls($"http://localhost:{port}")
+            .Configure(app =>
+            {
+                var staticFilesPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+
+                app.UseDefaultFiles(); // ищет index.html
+                app.UseStaticFiles(new StaticFileOptions
+                {
+                    FileProvider = new PhysicalFileProvider(staticFilesPath),
+                    RequestPath = "",
+                    ServeUnknownFileTypes = true
+                });
+
+                /*app.Run(async context =>
+                {
+                    await context.Response.WriteAsync("Hello from WPF + Kestrel!");
+                });*/
+            })
+            .Build();
+
+        host.RunAsync(); // запускаем в фоне, чтобы не блокировать UI
+    }
+}
--- ukichat-web/nuxt.config.ts
+++ ukichat-web/nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import {resolve} from 'path'
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/eslint']
+    // Режим сборки: static, если хочешь просто отдавать файлы
+    target: 'static',
+    // Путь, откуда будут браться файлы (вроде base URL)
+    /*router: {base: './'},*/
+    build: {sourcemap: false},
+    nitro: {
+        // Меняем папку, куда будут складываться публичные файлы
+        output: {
+            publicDir: resolve(__dirname, '../UkiChat/UkiChat/wwwroot')
+        }
+    },
+
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    modules: ['@nuxt/ui', '@nuxt/eslint']
 })
