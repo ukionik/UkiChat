4890c7e7865be09a160d5dcfd065e441e0acb11d
Vyzhanov Andrey <ukionik@yandex.ru>
2025-11-18 01:15:10 +0300

    ukichat-web Разбираемся с Nuxt UI и пробуем сделать поле "Канал"

---
+++ ukichat-web/app/components/HorizontalFormField.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface HorizontalFormFieldProps {
+  label?: string
+  name: string
+  [key: string]: any
+}
+
+const props = defineProps<HorizontalFormFieldProps>()
+</script>
+
+<template>
+  <UFormField :name="props.name" v-slot="{ field }">
+    <div class="flex items-center gap-4">
+      <label class="w-32 text-sm font-medium">{{ props.label }}</label>
+      <slot v-bind="field" />
+    </div>
+  </UFormField>
+</template>
--- ukichat-web/app/pages/settings.vue
+++ ukichat-web/app/pages/settings.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { useLocalization } from '~/composables/useLocalization'
+import * as v from 'valibot'
+import HorizontalFormField from "~/components/HorizontalFormField.vue";
 
 const { startSignalR, changeLanguage } = useLocalization()
 const { t } = useI18n()
+
+const schema = v.object({
+  settings: v.object({
+    twitch: v.object({
+      channel: v.pipe(v.string(), v.minLength(1, "Test"))
+    })
+  })
+})
+
+type Schema = v.InferOutput<typeof schema>
+
+const state = reactive({
+  settings:{
+    twitch: {
+      channel: '',
+    }
+  }
+})
 
 // Запуск SignalR при монтировании компонента
 onMounted(() => {
@@ -12,7 +32,12 @@ onMounted(() => {
 </script>
 
 <template>
-  <h1>{{ t('settings.title') }}</h1>
+  <UForm :schema="schema" :state="state" class="space-y-4">
+    <h2 class="text-xl font-semibold mb-4">Twitch</h2>
+    <HorizontalFormField label="Канал" name="channel">
+      <UInput v-model="state.settings.twitch.channel" />
+    </HorizontalFormField>
+  </UForm>
 </template>
 
 <style scoped>
