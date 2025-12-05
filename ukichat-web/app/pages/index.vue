e95bce06aa1eef77b51b06a0f99cbf9f0b51159e
Vyzhanov Andrey <ukionik@yandex.ru>
2025-12-05 03:00:06 +0300

    v1.0 Убран MetroApps (хрень)

--- ukichat-web/app/pages/index.vue
+++ ukichat-web/app/pages/index.vue
@@ -12,9 +12,6 @@ onMounted(() => {
 
 <template>
   <div>
-    <button>{{ t('settings.title') }}</button>
-<!--    <button @click="changeLanguage('ru')">RU</button>
-    <button @click="changeLanguage('en')">EN</button>-->
 <!--    <Icon icon="mdi:cog" width="24" height="24"/>-->
   </div>
 </template>
