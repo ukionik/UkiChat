e21e5adf0d313445ab21c078dab2c95832817a15
Vyzhanov Andrey <ukionik@yandex.ru>
2026-01-25 21:20:39 +0300

    v0.1 Чат вынесен в отдельный компонент ChatContainer

--- ukichat-web/app/pages/index.vue
+++ ukichat-web/app/pages/index.vue
@@ -17,35 +17,9 @@ const appSettingsInfo = ref({
 
 const maxMessages = 1000
 const chatMessages = ref<ChatMessage[]>([])
-const chatContainer = ref<HTMLElement | null>(null)
-const autoScroll = ref(true)
 
 function addItem(item: ChatMessage) {
   return [...chatMessages.value, item].slice(-maxMessages)
-}
-
-function scrollToBottom() {
-  if (!chatContainer.value) {
-    return
-  }
-
-  chatContainer.value.scrollTop = chatContainer.value.scrollHeight
-}
-
-function onScroll() {
-  const el = chatContainer.value
-  if (!el)
-    return
-
-  const threshold = 200
-  autoScroll.value = el.scrollTop + el.clientHeight >= el.scrollHeight - threshold
-}
-
-function getPlatformImage(platform: string) {
-  switch (platform) {
-    case "Twitch": return "/images/twitch.svg"
-    default: return ""
-  }
 }
 
 async function getActiveAppSettingsInfo() {
@@ -63,13 +37,6 @@ async function connectToTwitch() {
     console.error(error)
   }
 }
-
-watch(chatMessages, async () => {
-  await nextTick()
-  if (autoScroll.value) {
-    scrollToBottom()
-  }
-})
 
 // Запуск SignalR при монтировании компонента
 onMounted(async () => {
@@ -102,17 +69,5 @@ onMounted(async () => {
       </UIcon>
     </UButton>
   </div>
-  <div class="chat-container h-dvh overflow-y-auto" ref="chatContainer" @scroll="onScroll">
-    <div class="chat-message py-0.5" v-for="message in chatMessages">
-      <img class="inline h-5" :alt="message.platform" :src="getPlatformImage(message.platform)">
-      <img v-for="badge in message.badges" :key="badge" :src="badge" alt="badge" class="inline h-5 ml-1">
-      <span class="font-bold ml-1 align-middle">{{ message.displayName }}</span>
-      <span class="ml-1 inline">
-        <template v-for="(part, index) in message.messageParts" :key="index">
-          <span v-if="part.type === 'Text'" class="inline align-middle">{{ part.content }}</span>
-          <img v-else-if="part.type === 'Emote'" :src="part.content" alt="emote" class="inline h-7">
-        </template>
-      </span>
-    </div>
-  </div>
+  <ChatContainer :messages="chatMessages" />
 </template>
