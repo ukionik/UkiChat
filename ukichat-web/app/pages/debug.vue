d740aaccb2f7cfa02302c929eef504631d391f52
Vyzhanov Andrey <ukionik@yandex.ru>
2026-01-25 19:57:56 +0300

    v0.1 Текст теперь отображается корректно под иконками. Осталось отцентрировать

--- ukichat-web/app/pages/debug.vue
+++ ukichat-web/app/pages/debug.vue
@@ -6,7 +6,10 @@ const {startSignalR, invokeGet, invokeUpdate} = useSignalR()
 async function sendTestMessage() {
   await invokeUpdate("SendChatMessage", {
     displayName: "Player 1",
-    message: "Здарова!"
+    messageParts: [{
+      type: "Text",
+      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled"
+    }]
   })
 }
 
