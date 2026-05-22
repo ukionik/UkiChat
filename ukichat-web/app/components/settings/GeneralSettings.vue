97e37fc394a963b912778505658ffefa1e4246d5
Vyzhanov Andrey <ukionik@yandex.ru>
2026-05-22 22:36:31 +0300

    v0.4.0 Теперь высвечивается сообщение при копировании в буфер

--- ukichat-web/app/components/settings/GeneralSettings.vue
+++ ukichat-web/app/components/settings/GeneralSettings.vue
@@ -2,9 +2,14 @@
 const { t } = useI18n()
 
 const chatOverlayUrl = 'http://localhost:5000/chat'
+const copied = ref(false)
+let copiedTimer: ReturnType<typeof setTimeout>
 
 async function copyOverlayUrl() {
   await navigator.clipboard.writeText(chatOverlayUrl)
+  copied.value = true
+  clearTimeout(copiedTimer)
+  copiedTimer = setTimeout(() => { copied.value = false }, 2000)
 }
 </script>
 
@@ -14,15 +19,27 @@ async function copyOverlayUrl() {
       <label class="w-44 text-sm text-gray-400 shrink-0">
         {{ t('settings.general.chatOverlayUrl') }}
       </label>
-      <UInput :model-value="chatOverlayUrl" readonly class="flex-1 font-mono text-xs" />
-      <UButton
-        icon="i-heroicons-clipboard-document"
-        variant="ghost"
-        color="neutral"
-        size="sm"
-        :title="t('settings.general.copyToClipboard')"
-        @click="copyOverlayUrl"
-      />
+      <div class="relative flex flex-1">
+        <UInput
+          :model-value="chatOverlayUrl"
+          readonly
+          class="flex-1 font-mono text-xs [&_input]:rounded-r-none"
+        />
+        <UButton
+          icon="i-heroicons-clipboard-document"
+          variant="subtle"
+          color="neutral"
+          class="rounded-l-none border border-l-0 border-gray-700"
+          :title="t('settings.general.copyToClipboard')"
+          @click="copyOverlayUrl"
+        />
+        <span
+          class="absolute left-full ml-3 top-1/2 -translate-y-1/2 text-sm text-green-500 whitespace-nowrap transition-opacity duration-300"
+          :class="copied ? 'opacity-100' : 'opacity-0'"
+        >
+          {{ t('settings.general.copiedToClipboard') }}
+        </span>
+      </div>
     </div>
   </div>
 </template>
