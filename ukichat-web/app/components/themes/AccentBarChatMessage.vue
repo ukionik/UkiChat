594857e62e34e8d4a357d0247d8383b79638e6c9
Vyzhanov Andrey <ukionik@yandex.ru>
2026-07-29 13:23:01 +0300

    v0.6.3 - Доработки темы AccentBar

--- ukichat-web/app/components/themes/AccentBarChatMessage.vue
+++ ukichat-web/app/components/themes/AccentBarChatMessage.vue
@@ -14,15 +14,26 @@ const emit = defineEmits<{
 const { revealed, toggleRevealDeleted } = useThemeMessage(props, emit)
 
 const s = computed(() => props.scale)
+// Отступ между ником и текстом сообщения
+const marginRight = computed(() => `${0.25 * s.value}rem`)
 
-const boxStyle = computed(() => ({
-  padding: `${0.3 * s.value}rem ${0.65 * s.value}rem`,
-  marginBottom: `${0.32 * s.value}rem`,
-  fontSize: `${s.value}rem`,
-  background: 'rgba(0, 0, 0, 0.4)',
-  borderLeft: `${0.25 * s.value}rem solid ${getPlatformColor(props.message.platform)}`,
-  borderRadius: `0 ${0.4 * s.value}rem ${0.4 * s.value}rem 0`,
-}))
+const boxStyle = computed(() => {
+  const base: Record<string, string> = {
+    padding: `${0.3 * s.value}rem ${0.65 * s.value}rem`,
+    marginBottom: `${0.32 * s.value}rem`,
+    fontSize: `${s.value}rem`,
+    background: 'rgba(0, 0, 0, 0.9)',
+    borderLeft: `${0.25 * s.value}rem solid ${getPlatformColor(props.message.platform)}`,
+    borderRadius: `0 ${0.4 * s.value}rem ${0.4 * s.value}rem 0`,
+  }
+  const type = props.message.messageType
+  const variant = messageVariant(type)
+  if (variant === 'mention') base.background = 'rgb(113 11 5 / 0.9)'
+  else if (type === 'Notification' || type === 'Raid') base.background = 'rgba(94, 62, 0, 0.9)'
+  else if (type === 'ChannelPointsRedemption' || type === 'Subscription' || type === 'Cheer') base.background = 'rgb(66 13 89 / 0.9)'
+  else if (variant === 'event') base.background = 'rgb(0 81 35 / 0.9)'
+  return base
+})
 </script>
 
 <template>
@@ -35,8 +46,7 @@ const boxStyle = computed(() => ({
     <ChatMessageMeta :message="message" :scale="scale" />
     <div>
       <ChatPlatformBadges :message="message" :scale="scale" :icon-scale="1.15" inline />
-      <span class="font-bold align-middle" :style="{ color: message.displayNameColor }">{{ message.displayName }}:</span>
-      <span class="align-middle"> </span>
+      <span class="font-bold align-middle" :style="{ marginRight, color: message.displayNameColor }">{{ message.displayName }}:</span>
       <ChatMessageContent :message="message" :scale="scale" :allow-reveal-deleted="allowRevealDeleted"
                           :revealed="revealed" @link-click="emit('linkClick', $event)" />
     </div>
